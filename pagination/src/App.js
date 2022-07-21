820878c3574f3100e1b175727eaa190a2ac4fdd4
Bhavya Verma <vbhavya269@gmail.com>
2022-07-21 16:54:00 +0530

    🐛 Removed unwanted import statements

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -18,6 +17,8 @@ function App() {
 
     fetchPosts();
   }, []);
+
+  console.log(posts);
   
   return (
     <div>
