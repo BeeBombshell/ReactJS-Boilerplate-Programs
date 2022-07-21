007091fe1ab32814d928f2980b45c66e1ef56b5a
Bhavya Verma <vbhavya269@gmail.com>
2022-07-21 16:49:53 +0530

    ⚡️ Added states and state functions + useEffect

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
+      setPosts(res.data);
+      setLoading(false);
+    }
+
+    fetchPosts();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> My App </h1>
     </div>
   );
 }
