28f7f1877a66001e13156c6c3502e58e0e25e514
Bhavya Verma <vbhavya269@gmail.com>
2022-07-22 15:13:39 +0530

    ⚡️ Added page numbers in Pagination.js

---
+++ pagination/src/components/Pagination.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Pagination = ({ postsPerPage, totalPosts }) => {
+
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+  return (
+      <nav>
+          <ul className="pagination">
+              {pageNumbers.map(number => (
+                  <li key={number} className="page-item">
+                      <a href="!#" className="page-link">
+                        {number}
+                      </a>                      
+                  </li>
+              ))}
+          </ul>
+    </nav>
+  )
+}
